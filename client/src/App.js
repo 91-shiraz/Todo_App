6211b0118d3ab8a5fd9d04f7d85f82b66f4a8e2e
91-shirazm <sheerazshaikh965@gmail.com>
2023-05-30 03:38:41 +0500

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header";
+import TodoForm from "./Components/TodoForm";
+import Todos from  "./Components/Todos";
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <TodoForm/>
+    <Todos/>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+
+let Header = () =>{
+    return(
+        <header>
+            <h1>TODO APP</h1>
+        </header>
+    )
+}
+
+export default Header;
